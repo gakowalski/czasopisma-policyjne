7d279fd0c2ccc0430d4d972392d348c9ce03bf44
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-09-16 16:26:48 +0200

    Policja 997 Wydanie Specjalne

--- download-all.php
+++ download-all.php
@@ -1,5 +1,21 @@
 <?php
 
+$policja_997_wydanie_specjalne_linki = [
+  'http://www.gazeta.policja.pl/download/7/180717/Nr1specjalny072014.pdf' => array(1,2014,7),
+  'http://www.gazeta.policja.pl/download/7/180720/Nr2specjalny042015.pdf' => array(2,2015,4),
+  'http://www.gazeta.policja.pl/download/7/180722/Nr3specjalny072015.pdf' => array(3,2015,7),
+  'http://www.gazeta.policja.pl/download/7/180724/Nr4specjalny102015.pdf' => array(4,2015,10),
+  'http://www.gazeta.policja.pl/download/7/200132/Nr5specjalny062016.pdf' => array(5,2016,6),
+  'http://www.gazeta.policja.pl/download/7/209862/Nr6specjalny082016.pdf' => array(6,2016,8),
+  'http://www.gazeta.policja.pl/download/7/227163/Nr7specjalny122016.pdf' => array(7,2016,12),
+  'http://www.gazeta.policja.pl/download/7/228285/Nr8specjalny122016.pdf' => array(8,2016,12),
+  'http://www.gazeta.policja.pl/download/7/241415/Nr9specjalny042017.pdf' => array(9,2017,4),
+  'http://www.gazeta.policja.pl/download/7/266806/Nr1011specjalny122017.pdf' => array(10-11,2018,12),
+  'http://www.gazeta.policja.pl/download/7/288903/Nr12specjalny072018.pdf' => array(12,2018,7),
+  'http://www.gazeta.policja.pl/download/7/295802/Nr13specjalny092018.pdf' => array(13,2018,9),
+  'http://www.gazeta.policja.pl/download/7/301993/Nr14specjalny122018.pdf' => array(14,2018,12),
+];
+
 $policja_997_linki = array(
   'http://www.gazeta.policja.pl/download/7/318831/Nr172lipiec2019.pdf' => array(172,2019,7,0),
   'http://www.gazeta.policja.pl/download/7/316787/Nr171czerwiec2019.pdf' => array(171,2019,6,0),
@@ -535,6 +551,21 @@ foreach ($policja_997_linki as $url => $opis) {
   } else {
     $file = "Policja 997 - $rok-$miesiac nr $numer.pdf";
   }
+
+  if (file_exists($file) === false) {
+    echo "Pobieram $file\n";
+    download($url, $file);
+  } else {
+    echo "Pomijam $file\n";
+  }
+}
+
+foreach ($policja_997_wydanie_specjalne_linki as $url => $opis) {
+  $numer = $opis[0];
+  $rok = $opis[1];
+  $miesiac = $opis[2];
+
+  $file = "Policja 997 Wydanie Specjalne - $rok-$miesiac nr $numer.pdf";
 
   if (file_exists($file) === false) {
     echo "Pobieram $file\n";
